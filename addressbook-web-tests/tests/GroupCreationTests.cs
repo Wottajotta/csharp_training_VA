4bec92c53b0aa49ba235d480e43d855fcf572b1b
Vladislav <vlad.kharchenko.96@list.ru>
2026-01-21 17:34:19 +0500

    temp

--- addressbook-web-tests/tests/GroupCreationTests.cs
+++ addressbook-web-tests/tests/GroupCreationTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
 using WebAddressbookTests.tests;
+using System.IO;
 
 
 namespace WebAddressbookTests
@@ -23,7 +24,23 @@ namespace WebAddressbookTests
             return groups;
         }
 
-        [Test, TestCaseSource("RandomGroupDataProvider")]
+        public static IEnumerable<GroupData> GroupDataFromFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            string[] lines = File.ReadAllLines(@"groups.csv");
+            foreach(string l in lines)
+            {
+                string[] parts = l.Split(',');
+                groups.Add(new GroupData(parts[0])
+                {
+                    Header = parts[1],
+                    Footer = parts[2]
+                });
+            }
+            return groups;
+        }
+
+        [Test, TestCaseSource("GroupDataFromFile")]
         public void CreateGroupTest(GroupData group)
         {
 
